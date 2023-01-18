6f549f77e326c1c17b957c313279da4965aedaa7
jkoseki <66928602+jkoseki@users.noreply.github.com>
2023-01-18 22:07:50 +0900

    Add files via upload

---
+++ source/R-script/00-install-packages.R
@@ -0,0 +1,5 @@
+###  Install required packages
+##
+#
+
+install.packages(c("bio3d", "data.table", "dplyr", "earth", "gplots", "mclust", "openxlsx", "pheatmap", "readr", "Rtsne", "stringr", "TDA", "tidyverse"))
